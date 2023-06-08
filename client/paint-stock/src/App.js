964624067522f6db7782cea5784063edfb699d73
Ben H <benharrison7049@gmail.com>
2023-06-08 15:25:49 -0700

    created basic layout with swimlane component

--- client/paint-stock/src/App.js
+++ client/paint-stock/src/App.js
@@ -1,5 +1,19 @@
-function App() {
-  return <div></div>;
-}
+import { Container, Row, Col,} from "react-bootstrap";
+import "./App.css";
+import SwimLane from "./Components/SwimLane";
+const App = () => {
+  return (
+    <Container fluid>
+      <Row>
+        <Col className="content-col" xs={8}>
+          <h1>Paint Stock</h1>
+        </Col>
+        <Col className="content-col" xs={8}>
+          <SwimLane></SwimLane>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
 
 export default App;
---
+++ client/paint-stock/src/Components/SwimLane.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {Table } from "react-bootstrap";
+
+const SwimLane = () => {
+  return (
+    <Table>
+      <thead>
+        <tr>
+          <th className="col-4">Out of stock</th>
+          <th className="col-4">Low</th>
+          <th className="col-4">Available</th>
+        </tr>
+        <tr>
+        </tr>
+      </thead>
+      <tbody></tbody>
+    </Table>
+  );
+};
+
+export default SwimLane;
