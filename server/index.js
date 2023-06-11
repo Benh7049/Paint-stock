d379b586313bc6390997397ce7cdf2758ec9b11a
Ben H <benharrison7049@gmail.com>
2023-06-10 18:03:41 -0700

    implemented posting to dbwip

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require("express");
-const crypto = require('crypto')
+const crypto = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mysql = require("mysql2");
 const app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
+//todo add .env
+const db = mysql.createConnection({
+  user: "root",
+  host: "localhost",
+  password: "root",
+  database: "paintdb",
+});
 
 paintDict = {};
 
@@ -19,14 +27,24 @@ app.get("/api/paint", (req, res) => {
 
 app.post("/api/paint", (req, res) => {
   const { paintName, status } = req.body;
-  paintDict[crypto.randomBytes(20).toString('hex')] = {paintName,status}
+  db.query(
+    "INSERT INTO paints (paintName,status) VALUES (?,?)",
+    [paintName, status],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      }
+    }
+  );
 });
 
-app.delete("/api/paint/:id",(req,res)=>{
-  delete paintDict[String(req.params.id)]
-})
+app.delete("/api/paint/:id", (req, res) => {
+  delete paintDict[String(req.params.id)];
+  res.send(paintDict);
+});
 
-app.put("/api/paint/:id",(req,res)=>{
-  const {paintName,status} = req.body
-  paintDict[String(req.params.id)] = {paintName,status}
-})
+app.put("/api/paint/:id", (req, res) => {
+  const { paintName, status } = req.body;
+  paintDict[String(req.params.id)] = { paintName, status };
+  res.send(paintDict);
+});
